ba8c9364387d35b732f1e0196422c94bce41b0fe
Edu <eduardo.marin.contractor@dev.bbva.com>
2017-05-08 12:48:50 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+
+var clicked = {};
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+   if(changeInfo.status == "complete"){
+     chrome.browserAction.setIcon({path: "icon2.png"});
+     clicked[tabId] = clicked[tabId] || {};
+     clicked[tabId].loading = false;
+     clicked[tabId].opened = false;
+   }
+   if(changeInfo.status == "loading"){
+     clicked[tabId] = clicked[tabId] || {};
+     clicked[tabId].loading = true;
+     chrome.browserAction.setIcon({path: "icon2_disabled.png"});
+   }
+});
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+
+    chrome.tabs.query({
+        'active': true,
+        'lastFocusedWindow': true
+    }, function(tabs) {
+
+        if( !clicked[tabs[0].id].opened && !clicked[tabs[0].id].loading){
+            clicked[tabs[0].id].opened = true;
+            chrome.browserAction.setIcon({path: "icon2_disabled.png"});
+        }else{
+          return;
+        }
+
+        var _nocache = new Date().getTime();
+
+        chrome.tabs.executeScript(null, {
+
+            code: "var script = document.createElement('link');" +
+                "script.setAttribute('rel', 'stylesheet');" +
+                "script.setAttribute('href', 'main.css?" + _nocache +"');" +
+                "document.head.appendChild(script);"
+        });
+    });
+});
